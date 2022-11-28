f672d87e9771d6c02097c4115b5bfe2d53a69e90
hainguyen16051998 <114744307+hainguyen16051998@users.noreply.github.com>
2022-11-28 20:09:08 +0700

    qweqwe

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("222222222222");
         System.out.println("45555");
+        System.out.println("544444444444");
     }
 }
